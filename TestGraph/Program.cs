385e7a2738fe85c2072cf25277a647278283045d
lomination <lomination@rejna.org>
2025-02-25 22:09:02 +0100

    Use dotnet8 collection expression

--- TestGraph/Program.cs
+++ TestGraph/Program.cs
@@ -16,15 +16,15 @@ var resistor2 = new Resistor("R2", 150);
 var resistor3 = new Resistor("R3", 100);
 var switch1 = new Resistor("S", 0);
 
-var branchInN1Left = new BranchGraph(nodeIn, nodeN1, new List<IElecComponent>() { light1 });
-var branchInN1Right = new BranchGraph(nodeIn, nodeN1, new List<IElecComponent>() { resistor2 });
-var branchN1N2 = new BranchGraph(nodeN1, nodeN2, new List<IElecComponent>() { switch1 });
-var branchInN2 = new BranchGraph(nodeIn, nodeN2, new List<IElecComponent>() { resistor1, resistor3 });
-var branchN2Out = new BranchGraph(nodeN2, nodeOut, new List<IElecComponent>() { light2 });
+var branchInN1Left = new BranchGraph(nodeIn, nodeN1, [light1]);
+var branchInN1Right = new BranchGraph(nodeIn, nodeN1, [resistor2]);
+var branchN1N2 = new BranchGraph(nodeN1, nodeN2, [switch1]);
+var branchInN2 = new BranchGraph(nodeIn, nodeN2, [resistor1, resistor3]);
+var branchN2Out = new BranchGraph(nodeN2, nodeOut, [light2]);
 
-var graphInN1 = new ParallelGraph(nodeIn, nodeN1, new List<Graph>() { branchInN1Left, branchInN1Right });
-var graphInN2Right = new SeriesGraph(nodeIn, nodeN2, new List<Graph>() { graphInN1, branchN1N2 });
-var graphInN2 = new ParallelGraph(nodeIn, nodeN2, new List<Graph>() { branchInN2, graphInN2Right });
-var main = new SeriesGraph(nodeIn, nodeOut, new List<Graph>(){graphInN2, branchN2Out});
+var graphInN1 = new ParallelGraph(nodeIn, nodeN1, [branchInN1Left, branchInN1Right]);
+var graphInN2Right = new SeriesGraph(nodeIn, nodeN2, [graphInN1, branchN1N2]);
+var graphInN2 = new ParallelGraph(nodeIn, nodeN2, [branchInN2, graphInN2Right]);
+var main = new SeriesGraph(nodeIn, nodeOut, [graphInN2, branchN2Out]);
 
 main.LaunchEletrons(230, 16);
