f63aa60044a1ec036346aa256e26a958a5719a80
Nota-yob <142342012+Nota-yob@users.noreply.github.com>
2025-02-27 02:24:06 +0100

    Remove Lighton field, add Nominal tension, isLampOn method and doc

--- TestGraph/Components/Lamp.cs
+++ TestGraph/Components/Lamp.cs
@@ -3,16 +3,25 @@ using Reconnect.Electronics.Graph;
 
 namespace TestGraph.Components;
 
-public class Lamp : ElecComponent
+public class Lamp : Resistor
 {
-    public bool LightOn { get; private set; }
-    public Lamp(string name, int resistance) : base(name, resistance)
+    public int NominalTension;
+    public Lamp(string name, int resistance, int nominalTension) : base(name, resistance)
     {
-        LightOn = false;
+        NominalTension = nominalTension;
     }
 
-    public Lamp(string name, List<Vertice> adjacentComponents, int resistance) : base(name, adjacentComponents, resistance)
+    public Lamp(string name, List<Vertice> adjacentComponents, int resistance, int nominalTension) : base(name, adjacentComponents, resistance)
     {
-        LightOn = false;
+        NominalTension = nominalTension;
     }
+
+    /// <summary>
+    /// Determines whether the lamp is on or off when the given intensity is applied to it.
+    /// The lamps is turned on if the tension to its terminals is its <see cref="NominalTension"/>.
+    /// </summary>
+    /// <remarks>There is a tolerance of 0.1 Volts</remarks>
+    /// <param name="intensity">The intensity applied to the lamp</param>
+    /// <returns>true if the lamp turns on, false otherwise</returns>
+    public bool isLampOn(double intensity) => Math.Abs(GetVoltage(intensity) - NominalTension) < 0.1;
 }
